b2bd0303f4c2504853c8d88b9b660a56a55adcc1
Musah Abdul-Malik <musah.abdulmalikgh@gmail.com>
2021-04-30 09:33:37 +0000

    getting frontend ready

--- src/main.js
+++ src/main.js
@@ -1,9 +1,18 @@
 import Vue from 'vue'
 import App from './App.vue'
 import router from './router'
+import ElementUI from 'element-ui'
+import 'element-ui/lib/theme-chalk/index.css'
 
+import 'bootstrap'
+import 'bootstrap/dist/css/bootstrap.min.css'
+
+Vue.use(ElementUI)
 Vue.config.productionTip = false
 
+import axios from 'axios'
+
+Vue.prototype.$http = axios
 new Vue({
   router,
   render: h => h(App)
