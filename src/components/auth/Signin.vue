4abd7f7575edffab3d419b35b8de787a725b2879
Musah Abdul-Malik <musah.abdulmalikgh@gmail.com>
2021-04-30 10:29:23 +0000

    Added login ui

--- src/components/auth/Signin.vue
+++ src/components/auth/Signin.vue
@@ -4,24 +4,13 @@
           <div class="col-md-5 my-5">
             <div class="card">
                 <div class="card-header">
-                    <h2 class="text-center">Sign Up</h2>
+                    <h2 class="text-center">Login</h2>
                 </div>
                 <div class="card-body">
                     <el-form :model="form" :rules="rules" ref="ruleForm"
                         class="demo-ruleForm">
-                        <el-form-item label="First Name" prop="first_name"
-                            :rules="{required:true, message:'Please First Name is required', trigger: 'blur'}">
-                            <el-input v-model="form.first_name"></el-input>
-                        </el-form-item>
-                        <el-form-item label="Last Name" prop="last_name"
-                        :rules="{required:true, message:'Please Last Name is required', trigger: 'blur'}">
-                            <el-input v-model="form.last_name"></el-input>
-                        </el-form-item>
-                        <el-form-item label="Phone Number" prop="phone"
-                        :rules="{required:true, message:'Please Phone Number is required', trigger: 'blur'}">
-                            <el-input maxlength="10"  show-word-limit v-model="form.phone"></el-input>
-                        </el-form-item>
                         <el-form-item label="Email" prop="email"
+                        placeholder="Enter your email address"
                         :rules="[
                             {required:true, message:'Please Email is required', trigger: 'blur'},
                             {type:'email', message:'Enter a valid Email', trigger: 'blur'}
@@ -29,11 +18,12 @@
                             <el-input v-model="form.email"></el-input>
                         </el-form-item>
                         <el-form-item label="Password" prop="password"
+                        placeholder="Enter your password"
                         :rules="{required:true, message:'Please Password is required', trigger: 'blur'}">
                             <el-input v-model="form.password" show-password></el-input>
                         </el-form-item>
                         <p>
-                            I have an account? <a href="/login">Login</a>
+                            Don't have an account? <a href="/signup">Sign up</a>
                         </p>
                         <el-form-item>
                             <el-button style="float: right" type="primary" @click="submit('ruleForm')">Create</el-button>
@@ -52,16 +42,13 @@ export default {
     data() {
         return {
             form: {
-                first_name: '',
-                last_name: '',
                 email: '',
-                phone: '',
                 password: ''
             }
         }
     },
     methods: {
-
+        
         submit(formName) {
             this.$refs[formName].validate((valid) => {
 
--- src/router/index.js
+++ src/router/index.js
@@ -14,6 +14,14 @@ const routes = [
     component: () => import(/* webpackChunkName: "about" */ '../components/auth/Signup.vue')
   },
   {
+    path: '/login',
+    name: 'login',
+    // route level code-splitting
+    // this generates a separate chunk (about.[hash].js) for this route
+    // which is lazy-loaded when the route is visited.
+    component: () => import(/* webpackChunkName: "about" */ '../components/auth/Signin.vue')
+  },
+  {
     path: '/home',
     name: 'home',
     // route level code-splitting
